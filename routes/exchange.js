35266c013e5edb80a8691a569753c0aa572f0baa
S Shao <sshao14@gmail.com>
2018-03-04 21:35:44 -0500

    bug fex

--- routes/exchange.js
+++ routes/exchange.js
@@ -15,7 +15,7 @@ exports.listen = function(req, res) {
             conn.createChannel(function(err, ch) {
                 ch.assertExchange(ex, 'direct', {durable: false});
 
-                ch.assertQueue(ex, {exclusive: true}, function(err, q) {
+                ch.assertQueue('', {exclusive: true}, function(err, q) {
                     console.log(' [x] Awaiting requests');
 
                     req.body.keys.forEach(function(key) {
@@ -49,24 +49,23 @@ exports.speak = function(req, res) {
             conn.createChannel(function(err, ch) {
                 ch.assertExchange(ex, 'direct', {durable: false});
 
-                ch.assertQueue(ex, {exclusive: true}, function(err, q) {
+                ch.assertQueue('', {exclusive: true}, function(err, q) {
                     var corr = generateUuid();
                     console.log(' [x] Endpoint <speak> requesting %s: "%s"', 
                         req.body.key, req.body.msg);
 
                     ch.consume(q.queue, function(msg) {
                         if(msg.properties.correlationId == corr) {
-                            console.log(' [.] Got %s', msg.content.toString());
+                            console.log(' [.] Got %s',
+                            msg.content.toString());
                             res.send({
                                 'msg': msg.content.toString()
                             });
                             setTimeout(function() {
                                 conn.close();
-                                res.send({'status': 'ERROR'});
-                                process.exit(0);
                             }, 500);
                         }
-                    }, {noAck: true});
+                    }, {noAck: false});
 
                     ch.publish(ex, req.body.key, new Buffer(req.body.msg), 
                         {correlationId: corr, replyTo: q.queue});
